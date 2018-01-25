812d0fbaf6fa61b1a52a988dcb5a5a081ec431d1
sHale <sally.hale@homeport.org.uk>
2018-01-25 22:47:58 +0000

    Implement user search by postcode or town name with error handling

--- googlemaptest/src/App.js
+++ googlemaptest/src/App.js
@@ -10,21 +10,39 @@ class App extends Component {
   state = {
     averagePrice: 0,
     userInputPostcode: null,
-    showChart: false
+    showChart: false,
+    errorMsg: ''
   }
 
   handleSubmit = event => {
-    this.setState({userInputPostcode: event.target.value.toUpperCase()})
+    this.setState({
+      userInputPostcode: event.target.value.toUpperCase(),
+      errorMsg: ''
+    })
   }
+
+
 
   getAveragePriceByPostcode = (event) => {
     event.preventDefault()
     let postcode = this.state.userInputPostcode
+    if (postcode.match(/.\d/)) {
     return fetch(`https://peaceful-waters-20110.herokuapp.com/api/postcode/${postcode}/average_price`)
     .then(res => res.json())
     .then(res =>
-     this.setState({averagePrice: res[0].average}))
+      this.setState({averagePrice: res[0].average}))
+    } else {
+    return fetch(`https://peaceful-waters-20110.herokuapp.com/api/town/${postcode}/average_price`)
+    
+    .then(res => res.json())
+    .then(res =>
+      this.setState({averagePrice: res[0].average})
+    )
+    .catch(err =>
+      this.setState({errorMsg: 'information not found'}))
+    }
   }
+ 
 
 
   render() {
@@ -41,7 +59,8 @@ class App extends Component {
 
 
         <div className='inputrow'>
-          <form onSubmit={this.getAveragePriceByPostcode}><input className='postcodeInput' placeholder='enter postcode' type="text" maxlength = "8" onChange={this.handleSubmit} ></input></form>
+          <form onSubmit={this.getAveragePriceByPostcode}><input className='postcodeInput' placeholder='enter postcode' type="text" onChange={this.handleSubmit} ></input></form>
+          <p className='errHandle'>{this.state.errorMsg}</p>
         </div>
         <div className='inputrow2'>
           <select name="radius"><option>radius</option>
